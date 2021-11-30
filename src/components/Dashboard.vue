d6900aee850b998526e80299e97e3b3d833e57d0
akbarhlubis <alubis87@gmail.com>
2021-11-30 18:10:32 +0700

    nambah dashboard dikit

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,50 +1,85 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://literasinusantara.com/wp-content/uploads/2019/03/Mimpi-TBM-Kolong-2-720x720.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Mengejar Mimpi di Tengah Pandemi
-    </v-card-title>
-
-    <v-card-subtitle>
-      Karya: Michael Edward
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-spacer></v-spacer>
-
-      <v-btn
-        icon
-        @click="show = !show"
+  <div class="content">
+    <div class="md-layout">
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
       >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            src="https://literasinusantara.com/wp-content/uploads/2019/03/Mimpi-TBM-Kolong-2-720x720.jpg"
+            height="200px"
+          ></v-img>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+          <v-card-title> Mengejar Mimpi di Tengah Pandemi </v-card-title>
 
-        <v-card-text>
-          Buku ini menceritakan perjuangan seorang mahasiswa yang berjuang untuk mempertahankan prestasinya di tengah situasi pandemi covid 19
-        </v-card-text>
+          <v-card-subtitle> Karya: Michael Edward </v-card-subtitle>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+              <v-icon>{{
+                show ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                Buku ini menceritakan perjuangan seorang mahasiswa yang berjuang
+                untuk mempertahankan prestasinya di tengah situasi pandemi covid
+                19
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            src="https://literasinusantara.com/wp-content/uploads/2019/03/Mimpi-TBM-Kolong-2-720x720.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Mengejar Mimpi di Tengah Pandemi </v-card-title>
+
+          <v-card-subtitle> Karya: Michael Edward </v-card-subtitle>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+              <v-icon>{{
+                show ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                Buku ini menceritakan perjuangan seorang mahasiswa yang berjuang
+                untuk mempertahankan prestasinya di tengah situasi pandemi covid
+                19
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+
       </div>
-    </v-expand-transition>
-  </v-card>
+    </div>
+  </div>
 </template>
 
 
 
 <script>
-  export default {
-    data: () => ({
-      show: false,
-    }),
-  }
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
 </script>
