9da050e0977849a0f50c465765dc9fea086e42ff
akbarhlubis <alubis87@gmail.com>
2021-12-02 12:52:52 +0700

    memperbaiki tampilan dashboard

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="dashboard">
     <v-navigation-drawer v-model="drawer" class="fullheight" width="256" app>
+
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title"> PerpustakaanKita </v-list-item-title>
@@ -9,26 +10,40 @@
       </v-list-item>
       
       <v-divider></v-divider>
-      <v-list dense nav>
-        <v-list-item
+      <v-list dense nav v-if="loggedIn && loggedinAdmin" >
+        <v-list-item 
           v-for="item in items"
           :key="item.title"
           link
           tag="router-link"
           :to="item.to"
         >
-          <v-list-item-content>
+        <v-list-item-content>
             <v-icon >{{item.icon}}</v-icon>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
 
+      <v-list dense nav v-if="loggedIn && !loggedinAdmin" >
+        <v-list-item 
+          v-for="item in user"
+          :key="item.title"
+          link
+          tag="router-link"
+          :to="item.to"
+        >
+        <v-list-item-content>
+            <v-icon >{{item.icon}}</v-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+
     </v-navigation-drawer>
     <v-app-bar app fixed height="75px">
-      <v-app-bar-nav-icon color = "yellow" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <!-- <v-img src=""></v-img> -->
-      <v-toolbar-title>PerpustakaanKita</v-toolbar-title>
+      <v-app-bar-nav-icon color = "green" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <VSpacer />
       <v-toolbar-items>
         <v-btn text router color = "blue" @click = "btnProfile"><v-icon>mdi-account</v-icon></v-btn>
@@ -48,7 +63,7 @@ export default {
   data() {
     return {
       drawer: true,
-      items: [
+       items: [
         { title: "Dashboard", to: "/dashboard", icon: "mdi-view-dashboard"},
         { title: "Daftar Pengguna", to: "/user",icon: "mdi-account-group"},
         { title: "Buku", to:"/buku",icon:"mdi-book" },
@@ -56,6 +71,13 @@ export default {
         { title: "Peminjaman", to:"/peminjaman",icon:"mdi-clock-time-nine"},
         { title: "Pengembalian", to:"/pengembalian",icon:"mdi-hand-heart"},
       ],
+      user: [
+        { title: "Dashboard", to: "/dashboard", icon:"mdi-home"},
+        { title: "List Buku", to:"/bukuuser", icon:"mdi-book"},
+        { title: "Denda Pemustaka", to:"/dendauser", icon:"mdi-currency-usd"},
+        { title: "Peminjaman", to:"/peminjaman", icon:"mdi-clock-time-nine"},
+        { title: "Pengembalian", to:"/pengembalian", icon:"mdi-hand-heart"},
+      ]
     };
   },
   methods: {
@@ -67,6 +89,14 @@ export default {
       localStorage.removeItem('token');
       location.reload();
     },
+  },
+  computed: {
+    loggedIn(){
+      return localStorage.getItem("token") != null;
+    },
+    loggedinAdmin(){
+      return localStorage.getItem("id") == "4";
+    }
   }
 };
 </script>
