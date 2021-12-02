fb97f19fc14333ce510b43810468fdc4cf596e75
akbarhlubis <alubis87@gmail.com>
2021-12-02 12:20:26 +0700

    memperbarui sidebar layout

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="dashboard">
     <v-navigation-drawer v-model="drawer" class="fullheight" width="256" app>
-
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title"> PerpustakaanKita </v-list-item-title>
@@ -10,46 +9,26 @@
       </v-list-item>
       
       <v-divider></v-divider>
-      <v-list dense nav v-if="loggedIn && loggedinAdmin" >
-        <v-list-item 
+      <v-list dense nav>
+        <v-list-item
           v-for="item in items"
           :key="item.title"
           link
           tag="router-link"
           :to="item.to"
         >
-              <v-list-item-icon >
-                  <v-icon v-text="item.icon" class="icon"></v-icon>
-                </v-list-item-icon>
           <v-list-item-content>
-               
-            <v-list-item-title class="text-left">{{ item.title }}</v-list-item-title>
+            <v-icon >{{item.icon}}</v-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-
-      <v-list dense nav v-if="loggedIn && !loggedinAdmin" >
-        <v-list-item 
-          v-for="item in user"
-          :key="item.title"
-          link
-          tag="router-link"
-          :to="item.to"
-        >
-        <v-list-item-icon >
-                <v-icon v-text="item.icon" class="icon"></v-icon>
-              </v-list-item-icon>
-          <v-list-item-content>
-              
-            <v-list-item-title class="text-left">{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
 
     </v-navigation-drawer>
     <v-app-bar app fixed height="75px">
-      <v-app-bar-nav-icon color = "green" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon color = "yellow" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <!-- <v-img src=""></v-img> -->
+      <v-toolbar-title>PerpustakaanKita</v-toolbar-title>
       <VSpacer />
       <v-toolbar-items>
         <v-btn text router color = "blue" @click = "btnProfile"><v-icon>mdi-account</v-icon></v-btn>
@@ -70,20 +49,13 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: "Dashboard", to: "/dashboard", icon:"mdi-home"},
-        { title: "Daftar Pengguna", to: "/user", icon:"mdi-account" },
-        { title: "Buku", to:"/buku" },
-        { title: "Denda", to:"/denda"},
-        { title: "Peminjaman", to:"/peminjaman"},
-        { title: "Pengembalian", to:"/pengembalian"},
+        { title: "Dashboard", to: "/dashboard", icon: "mdi-view-dashboard"},
+        { title: "Daftar Pengguna", to: "/user",icon: "mdi-account-group"},
+        { title: "Buku", to:"/buku",icon:"mdi-book" },
+        { title: "Denda", to:"/denda",icon:"mdi-currency-usd"},
+        { title: "Peminjaman", to:"/peminjaman",icon:"mdi-clock-time-nine"},
+        { title: "Pengembalian", to:"/pengembalian",icon:"mdi-hand-heart"},
       ],
-      user: [
-        { title: "Dashboard", to: "/dashboard", icon:"mdi-home"},
-        { title: "List Buku", to:"/bukuuser", icon:"mdi-book-open"},
-        { title: "Denda Pemustaka", to:"/dendauser"},
-        { title: "Peminjaman", to:"/peminjaman"},
-        { title: "Pengembalian", to:"/pengembalian"},
-      ]
     };
   },
   methods: {
@@ -95,14 +67,6 @@ export default {
       localStorage.removeItem('token');
       location.reload();
     },
-  },
-  computed: {
-    loggedIn(){
-      return localStorage.getItem("token") != null;
-    },
-    loggedinAdmin(){
-      return localStorage.getItem("id") == "4";
-    }
   }
 };
 </script>
